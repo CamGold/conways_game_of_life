0603991cbf572c6d64d0b0351cb5ea7b7c4e125c
Cameron Gold <cagold022@student.wethinkcode.co.za>
2023-05-29 12:17:00 +0200

    added basic square drawing

--- GameOfLife/src/main/java/org/example/GamePanel.java
+++ GameOfLife/src/main/java/org/example/GamePanel.java
@@ -2,17 +2,52 @@ package org.example;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class GamePanel extends JPanel{
     static final int WIDTH = 800;
     static final int HEIGHT = 800;
     static final int UNIT_SIZE = 25;
     static final int GAME_UNITS = (WIDTH*HEIGHT)/UNIT_SIZE;
+
+    int squareX;
+    int squareY;
     boolean running = true;
     GamePanel() {
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
         this.setBackground(Color.BLACK);
         this.setFocusable(true);
+        this.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                squareX = e.getX();
+                squareY = e.getY();
+                System.out.println(squareX + " " + squareY);
+                repaint();
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
     }
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -25,6 +60,15 @@ public class GamePanel extends JPanel{
                 g.drawLine(i * UNIT_SIZE, 0, i*UNIT_SIZE, HEIGHT);
                 g.drawLine(0, i * UNIT_SIZE, WIDTH, i * UNIT_SIZE);
             }
+            drawSquare(g);
         }
     }
+
+    public void drawSquare(Graphics g) {
+        squareX = 25*(Math.round((float) squareX /25));
+        squareY = 25*(Math.round((float) squareY /25));
+        g.setColor(Color.WHITE);
+        g.fillRect(squareX, squareY, UNIT_SIZE, UNIT_SIZE);
+    }
+
 }
